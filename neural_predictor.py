9c18ddfbcc63c4e880d1f1e3d622616655d78ef9
PATARA TRIRAT <patara.t@ku.th>
2021-05-21 02:58:03 +0900

    update project outline

--- neural_predictor.py
+++ neural_predictor.py
@@ -1,2 +1,10 @@
-def neural_predictor(n_layers, n_filters, two_stage=False):
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+
+def gcn_regressor(n_layers, n_filters):
+    return model
+
+def gcn_classifier(n_layers, n_filters):
     return model
---
+++ search_spaces.py
@@ -0,0 +1,10 @@
+
+def NAS_Bench_101():
+    return data
+
+
+def NAS_Bench_201():
+    return data
+
+def ProxylessNAS():
+    return data
